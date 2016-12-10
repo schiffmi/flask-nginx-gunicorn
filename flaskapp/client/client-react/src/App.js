57a5999470b606160d126931b7ff269fcea0d8f4
schiffmi <schiff.mi@gmail.com>
2016-12-10 16:56:41 -0500

    Starting to build out components

--- flaskapp/client/client-react/src/App.js
+++ flaskapp/client/client-react/src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+// import Spells from './components/spells';
+import Monsters from './components/monsters';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   constructor(props) {
@@ -12,7 +15,7 @@ class App extends Component {
     axios.get('api/monsters')
       .then(response => this.setState({monsters: response.data.monsters }) )
       .catch(e => console.log('Error', e));
-    axios.get('api/spells') 
+    axios.get('api/spells')
       .then(response => this.setState({spells: response.data.spells }) )
       .catch(e => console.log('Error', e));
 
@@ -25,9 +28,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Monsters monsters={this.state.monsters} />
+        {/*<Spells spells={this.state.spells}/>*/}
       </div>
     );
   }
---
+++ flaskapp/client/client-react/src/components/monsters.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Monster from './monster';
+
+class Monsters extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {filter: ''};
+	}
+
+	render() {
+		const monsters = this.props.monsters;
+		return (
+			<div>
+				{ monsters.map( monster =>  ( <Monster id={monster.name} monster={monster} /> )) }
+			</div>
+		);
+	}
+}
+
+Monsters.propTypes = {
+	monsters: React.PropTypes.array
+};
+
+export default Monsters;
